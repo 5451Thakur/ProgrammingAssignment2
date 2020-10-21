a723fa03dc453c1b178c9e7d2c344a987cd32757
5451Thakur <60716470+5451Thakur@users.noreply.github.com>
2020-10-21 14:27:47 +0200

    Update cachematrix.R
    Committed with corrections on variable names

--- cachematrix.R
+++ cachematrix.R
@@ -6,8 +6,8 @@
 
 ## makeCacheMatrix
 ##  Creates a special "matrix", which is really a list containing a functions to:
-##    set the value of the vector
-##    get the value of the vector
+##    set the value of the matrix
+##    get the value of the matrix
 ##    set the value of the inverse of the matrix
 ##    get the value of the inverse of the matrix
 
